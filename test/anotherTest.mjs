f698762e38f9a614c2c962c77c62dbcba5f038ab
Akash Anand <akash10.anand@gmail.com>
2021-04-17 22:40:28 +0530

    Added example of how this lib can be used as ES6 import

--- test/anotherTest.mjs
+++ test/anotherTest.mjs
@@ -1,5 +1,28 @@
-import {Group, Test, Hook} from "../src/test";
+/**
+ * This is an example of ES6 type usage of the Test-library.
+ */
 
-const group = new Group();
+import { group, test, step, before, beforeEach, afterEach, after, runGroups } from '../src/jstest.js';
+import { promisify } from 'util';
+import { ok } from 'assert';
 
-group.before = new Hook()
+const sleep = promisify(setTimeout);
+
+group("A basic group of tests", {
+    tests: [
+        test("inner test 1", async (t) => {
+            await sleep(1000);
+        }, {
+            skip: false,
+            retry: 2,
+            parallel: true
+        }),
+
+        test("inner test 2", async (t) => {
+            await sleep(1000);
+            ok(1 === 1)
+        })
+    ]
+
+})
+runGroups();
